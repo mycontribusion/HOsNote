474363afa65f656c02e97cd18b5c68a1e135a59f
Ahmad Musa <mycontribusion01@gmail.com>
2026-08-08 21:56:47 +0100

    url include all

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/team/:tab" element={<App />} />
                 <Route path="/notebook" element={<App />} />
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
         <Analytics />
         <SpeedInsights />
     </React.StrictMode>,
